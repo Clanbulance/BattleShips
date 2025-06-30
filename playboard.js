400d0b0c5174f24e7bd2b38a1413477811faa3db
Clanbulance <107610779+Clanbulance@users.noreply.github.com>
2025-06-30 18:54:53 +0200

    next step is to change require to modules

--- playboard.js
+++ playboard.js
@@ -3,3 +3,8 @@ const gameBoard = require("./gameBoard")
 const player = require("./players.js")
 
 
+const userInput = document.querySelector("#username")
+
+userInput.style.color  = "red";
+
+console.log(userInput)
