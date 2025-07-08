ec834696b521914804185d2391e7a1cca548585e
Clanbulance <107610779+Clanbulance@users.noreply.github.com>
2025-07-08 16:50:10 +0200

    adding new module for dom

---
+++ domManager.js
@@ -0,0 +1,15 @@
+import ship from "./ship.js";
+import gameBoard from "./gameBoard.js";
+import player from "./players.js";
+
+let p1Name,p2Name = ""
+const userInput = document.querySelector("#username")
+
+function consolePlayers(){
+    p1Name = new player(userInput.value,"human")
+    p2Name = new player("botMeister234","bot")
+    console.log(`Player has chosen : ${p1Name.name}`)
+    console.log(`He will be facing: ${p2Name.name}`)
+}
+
+export default consolePlayers
