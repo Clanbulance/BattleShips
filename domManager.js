432b9caf3c57e0f8bb585b09569bc2f083c217a6
Clanbulance <107610779+Clanbulance@users.noreply.github.com>
2025-07-10 15:29:24 +0200

    added createPanelContent & createGameContainer

--- domManager.js
+++ domManager.js
@@ -6,10 +6,14 @@ let p1Name,p2Name = ""
 const userInput = document.querySelector("#username")
 const userpanel = document.querySelector("#userpanel")
 const botpanel = document.querySelector("#botpanel")
+const players = [];
 
+// create the players from the inputfields and add them to the array
 function consolePlayers(){
     p1Name = new player(userInput.value,"human")
+    players.push(p1Name)
     p2Name = new player("botMeister234","bot")
+    players.push(p2Name)
     console.log(`Player has chosen : ${p1Name.name}`)
     console.log(`He will be facing: ${p2Name.name}`)
 }
@@ -23,17 +27,45 @@ Then we want to make it so we have a grid for both players
 
 */
 // helperfunction to test
-function createPanelContent(){
+function createPanelContent(playerobjectname){
     const namefield = document.createElement('div');
     namefield.classList.add("name")
-
     const playbox = document.createElement('div');
     playbox.classList.add("playbox")
-
+    playbox.id =  playerobjectname
     return [namefield,playbox]
 }
 
-function initiateGame(){
+//generate a gamecontainer element
+function createGameContainer(playername){
+
+    const gameContainer = document.createElement("div");
+    const scoreContainer = document.createElement("div");
+
+    gameContainer.classList.add("gameContainer");
+    scoreContainer.classList.add("scoreContainer");
+
+    gameContainer.id = playername+"-grid"
+    scoreContainer.id = playername+"score"
+
+    return [gameContainer,scoreContainer]
+}
+
+
+
+//Both players havea seperate gameboard. We need to create visuals for these arrays inside of:
+// "#adad-grid" *& #p2Name.namn+"grid"
+function createBoxFromArray(array){
+   array.forEach(player => console.log(player.board)) 
+
+   //set variables
+   const p1grid = document.querySelector(`#${p1Name.name}-grid`)
+   const p2grid = document.querySelector(`#${p2Name.name}-grid`)
+
+
+}
+
+function initiateGUI(){
     console.log("starting the initiateGame function")
 
     // clear boxes
@@ -41,8 +73,8 @@ function initiateGame(){
     botpanel.innerHTML = "";
 
     // call the helper function to generate divs
-    userpanel.append(...createPanelContent());
-    botpanel.append(...createPanelContent());
+    userpanel.append(...createPanelContent(p1Name.name));
+    botpanel.append(...createPanelContent(p2Name.name));
 
     // add the usernames to the topfield
     document.querySelector(".userpanel .name").textContent = p1Name.name
@@ -52,7 +84,11 @@ function initiateGame(){
     userpanel.classList = "gameGUI"
     botpanel.classList = "gameGUI"
 
+    //call the helperfunction to create gameContainer element on :
+    // #userpanel p1Name.name / #botpanel p2Name.name
+    document.querySelector("#userpanel .playbox").append(...createGameContainer(p1Name.name))
+    document.querySelector("#botpanel .playbox").append(...createGameContainer(p2Name.name))
 
 }
 
-export  {consolePlayers,initiateGame}
+export  {consolePlayers,initiateGUI,createBoxFromArray, players}
